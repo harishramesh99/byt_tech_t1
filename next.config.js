e3cb9f08c12893bcbfff6fcd81b9130e388a378c
harishramesh99 <harish.ramesh99@gmail.com>
2024-12-19 15:47:01 -0600

    fix Pid v2

--- next.config.js
+++ next.config.js
@@ -11,4 +11,17 @@ module.exports = {
       },
     ],
   },
+  async headers() {
+    return [
+      {
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Methods", value: "GET,POST,OPTIONS" },
+        ],
+      },
+    ]
+  }
+
 };
--- sanity.config.ts
+++ sanity.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "sanity";
 import { deskTool } from "sanity/desk";
+import {visionTool} from '@sanity/vision'
 import { schemaTypes } from "./schemas";
 
 export default defineConfig({
@@ -8,6 +9,6 @@ export default defineConfig({
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || "",
   dataset: "production",
   basePath: "/studio",
-  plugins: [deskTool()],
+  plugins: [deskTool(), visionTool()],
   schema: { types: schemaTypes },
 });
